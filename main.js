6ea0ab5d9a9fa0017475b04438e1a9682b6e8184
Poshika-alt <76203042+Poshika-alt@users.noreply.github.com>
2021-05-28 19:51:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function gotPoses(results){
 
 function draw(){
 image(video,0,0,300,300);
-image(clown_nose,noseX,noseY,30,30);
+image(clown_nose,noseX-10,noseY,30,30);
 }
 
 function take_snapshot(){
